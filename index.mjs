06395f61a24875c03db1a2323d78952661dacf75
CARLOS <carlos012100@gmail.com>
2025-05-08 08:50:41 +0200

    [CREATE]array object of CHARACTERS according to JSON

--- index.mjs
+++ index.mjs
@@ -1,4 +1,26 @@
 import Character from "./Character.mjs";
 import Day from "./Day.mjs";
 import Weapon from "./Weapon.mjs";
-import CharacterBD from "./data/CharacterBD.mjs";
+import characterBD from "./data/characterBD.mjs";
+
+const charactersArray = [];
+characterObjectArray();
+console.log(charactersArray)
+
+function characterObjectArray()
+{
+    for (let i = 0; i < characterBD.length; i++)
+    {
+        const character = new Character(
+            characterBD[i].name,
+            characterBD[i].occupation,
+            characterBD[i].gold,
+            characterBD[i].level,
+            characterBD[i].skills,
+            characterBD[i].weapons
+        );
+        charactersArray.push(character)
+    }
+    return charactersArray;
+
+}
