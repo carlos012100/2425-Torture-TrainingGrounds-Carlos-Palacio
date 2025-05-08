8c3754c61380cc4967fe4f26835da4ce8be5d147
CARLOS <carlos012100@gmail.com>
2025-05-08 10:49:07 +0200

    [CREATE] trainsimulator/print data

--- Character.mjs
+++ Character.mjs
@@ -10,6 +10,94 @@ export default class Character {
     }
     trainForDay(weaponName, dayObj)
     {
+        let dice3faces = Math.floor(Math.random()* 3);
+        let dice2faces = Math.floor(Math.random()*2);
+        let points = 0;
+
+        if (this.occupation === "thug")
+        {
+            if(this.weapon.type === "missile")
+            {
+                const modifier = + 2
+                 points = Math.floor((this.level/2)) + dice3faces + modifier;
+
+            }
+            else {
+                const modifier = - 2
+                 points = Math.floor((this.level/2)) + dice3faces + modifier;
+
+            }
+
+        }
+        else if(this.occupation === "priest")
+        {
+            if(this.weapon.type === "missile")
+            {
+                const modifier = 2;
+                 points = this.level + dice3faces + modifier;
+            }
+            if(this.weapon.type === "brawl")
+            {
+                let modifier = -1;
+                
+                if (Math.random()< 0.2)
+                {
+                    modifier = -2;
+                }
+                else{
+                    modifier = -1;
+                }
+                 points = this.level + dice3faces + modifier;
+
+            }
+            else{
+                const modifier = - 2
+                 points = this.level + dice3faces + modifier;
+            }
+        }
+        else if (this.occupation === "peasant" && dayObj.name === "wednesday") {
+            if (this.weapon.type === "brawl")
+            {
+                 points = dice2faces + Math.floor(this.level/2);
+            }
+        }
+
+
+        this.weapon.points += points;
+
+        if(this.weapon.points === this.weapon.points_for_next_skill)
+        {
+            let type = this.weapon.type;
+
+            this.skills = type;
+
+            type += 1;
+            this.weapon.points_for_next_skill = currentPointsNextSKill;
+            this.weapon.points_for_next_skill = currentPointsNextSKill + Math.floor(this.skills/5);
+
+            this.weapon.points = 0;
+
+        }
+        else if(this.weapon.points > this.weapon.points_for_next_skill)
+        {
+            let type = this.weapon.type;
+            this.skills = type;
+            type +=1;
+            this.weapon.points = this.weapon.points%this.weapon.points_for_next_skill;
+
+            this.weapon.points_for_next_skill = currentPointsNextSKill;
+            this.weapon.points_for_next_skill = currentPointsNextSKill + Math.floor(this.skills/5);
+
+            this.weapon.points = 0;
+
+
+        }
+        else {
+            this.weapon.points = 0;
+        }
+
+        this.gold -= 50;
 
     }
+
 }
--- index.mjs
+++ index.mjs
@@ -27,4 +27,25 @@ characterObjectArray();
 
 //Create obj Day
 
-const day = new Day(3 , "tuesday")
+const day = new Day(3 , "Tuesday");
+
+//create sim training
+
+charactersArray[1].trainForDay(charactersArray[1].weapon.name, day)
+console.log(charactersArray[1].weapon)
+
+function printData()
+{
+    console.log(day.number + "," + day.name);
+    console.log("-------------")
+    console.log("")
+    for(let i = 0; i < charactersArray.length; i++)
+    {
+        console.log(charactersArray[i].name + ", " + "a " + charactersArray[i].occupation + ", " + "begins the training")
+        console.log("current skills: " + "\n" + "- " + charactersArray[i].skills);
+        console.log(charactersArray[i].name + "trains the" + charactersArray[i].weapon.type + "skill" + "by using" + charactersArray[i].weapon.name)
+        console.log("current weapon points: " + charactersArray[i].weapon.points)
+        console.log("weapon points to increase skill: " + charactersArray[i].pointsForNextSkill)
+    }
+}
+printData();
